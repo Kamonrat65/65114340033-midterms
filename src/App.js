65986cde3259bf45c9dd0bea95ae2515143bb030
Kamonrat65 <136230822+Kamonrat65@users.noreply.github.com>
2023-08-25 16:11:43 +0700

    update

--- src/App.js
+++ src/App.js
@@ -132,7 +132,7 @@ function App() {
     <BrowserRouter basename="/65114340033-midterm/">
       <Container className="p-3 my-3 bg-light">
         <Routes>
-          <Route path="" element={<Index />} />
+          <Route path="/" element={<Index />} />
           <Route path="/main" element={<Index />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
